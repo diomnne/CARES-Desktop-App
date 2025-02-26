524567711ed9f98e06c703bc236686c23c702481
Frances Dionne Maglasang <dionnemaglasang@gmail.com>
2025-02-26 22:27:50 +0800

    Update login UI

--- CARES/log_in.Designer.cs
+++ CARES/log_in.Designer.cs
@@ -39,15 +39,15 @@
             this.ll_signup = new Guna.UI.WinForms.GunaLinkLabel();
             this.lbl_account = new Guna.UI.WinForms.GunaLabel();
             this.gunaPanel1 = new Guna.UI.WinForms.GunaPanel();
+            this.btn_minimize = new Guna.UI.WinForms.GunaButton();
+            this.btn_exit = new Guna.UI.WinForms.GunaButton();
+            this.gunaTransfarantPictureBox1 = new Guna.UI.WinForms.GunaTransfarantPictureBox();
             this.gunaShadowPanel1 = new Guna.UI.WinForms.GunaShadowPanel();
             this.btn_view1 = new Guna.UI.WinForms.GunaImageButton();
             this.btn_hide1 = new Guna.UI.WinForms.GunaImageButton();
-            this.btn_exit = new Guna.UI.WinForms.GunaButton();
-            this.gunaTransfarantPictureBox1 = new Guna.UI.WinForms.GunaTransfarantPictureBox();
-            this.btn_minimize = new Guna.UI.WinForms.GunaButton();
             this.gunaPanel1.SuspendLayout();
-            this.gunaShadowPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.gunaTransfarantPictureBox1)).BeginInit();
+            this.gunaShadowPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lbl_password_error
@@ -56,7 +56,7 @@
             this.lbl_password_error.BackColor = System.Drawing.Color.White;
             this.lbl_password_error.Font = new System.Drawing.Font("Segoe UI", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_password_error.ForeColor = System.Drawing.Color.Red;
-            this.lbl_password_error.Location = new System.Drawing.Point(147, 515);
+            this.lbl_password_error.Location = new System.Drawing.Point(147, 739);
             this.lbl_password_error.Name = "lbl_password_error";
             this.lbl_password_error.Size = new System.Drawing.Size(149, 30);
             this.lbl_password_error.TabIndex = 31;
@@ -69,7 +69,7 @@
             this.lbl_email_error.BackColor = System.Drawing.Color.White;
             this.lbl_email_error.Font = new System.Drawing.Font("Segoe UI", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_email_error.ForeColor = System.Drawing.Color.Red;
-            this.lbl_email_error.Location = new System.Drawing.Point(147, 358);
+            this.lbl_email_error.Location = new System.Drawing.Point(147, 582);
             this.lbl_email_error.Name = "lbl_email_error";
             this.lbl_email_error.Size = new System.Drawing.Size(113, 30);
             this.lbl_email_error.TabIndex = 30;
@@ -87,7 +87,7 @@
             this.txt_password.FocusedForeColor = System.Drawing.SystemColors.ControlText;
             this.txt_password.Font = new System.Drawing.Font("Century Gothic", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txt_password.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(57)))), ((int)(((byte)(55)))));
-            this.txt_password.Location = new System.Drawing.Point(143, 447);
+            this.txt_password.Location = new System.Drawing.Point(143, 671);
             this.txt_password.MaxLength = 255;
             this.txt_password.Name = "txt_password";
             this.txt_password.PasswordChar = '●';
@@ -107,7 +107,7 @@
             this.txt_email.FocusedForeColor = System.Drawing.SystemColors.ControlText;
             this.txt_email.Font = new System.Drawing.Font("Century Gothic", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txt_email.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(57)))), ((int)(((byte)(55)))));
-            this.txt_email.Location = new System.Drawing.Point(143, 290);
+            this.txt_email.Location = new System.Drawing.Point(143, 514);
             this.txt_email.MaxLength = 255;
             this.txt_email.Name = "txt_email";
             this.txt_email.PasswordChar = '\0';
@@ -122,7 +122,7 @@
             this.lbl_password.BackColor = System.Drawing.Color.White;
             this.lbl_password.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_password.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(57)))), ((int)(((byte)(55)))));
-            this.lbl_password.Location = new System.Drawing.Point(146, 411);
+            this.lbl_password.Location = new System.Drawing.Point(146, 635);
             this.lbl_password.Name = "lbl_password";
             this.lbl_password.Size = new System.Drawing.Size(115, 32);
             this.lbl_password.TabIndex = 29;
@@ -134,7 +134,7 @@
             this.lbl_email.BackColor = System.Drawing.Color.White;
             this.lbl_email.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_email.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(57)))), ((int)(((byte)(55)))));
-            this.lbl_email.Location = new System.Drawing.Point(146, 253);
+            this.lbl_email.Location = new System.Drawing.Point(146, 477);
             this.lbl_email.Name = "lbl_email";
             this.lbl_email.Size = new System.Drawing.Size(72, 32);
             this.lbl_email.TabIndex = 28;
@@ -146,7 +146,7 @@
             this.lbl_create_account.BackColor = System.Drawing.Color.White;
             this.lbl_create_account.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_create_account.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(57)))), ((int)(((byte)(55)))));
-            this.lbl_create_account.Location = new System.Drawing.Point(359, 132);
+            this.lbl_create_account.Location = new System.Drawing.Point(359, 356);
             this.lbl_create_account.Name = "lbl_create_account";
             this.lbl_create_account.Size = new System.Drawing.Size(215, 77);
             this.lbl_create_account.TabIndex = 32;
@@ -163,7 +163,7 @@
             this.btn_login.ForeColor = System.Drawing.Color.White;
             this.btn_login.Image = null;
             this.btn_login.ImageSize = new System.Drawing.Size(20, 20);
-            this.btn_login.Location = new System.Drawing.Point(250, 603);
+            this.btn_login.Location = new System.Drawing.Point(250, 827);
             this.btn_login.Name = "btn_login";
             this.btn_login.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(190)))), ((int)(((byte)(196)))));
             this.btn_login.OnHoverBorderColor = System.Drawing.Color.Black;
@@ -182,7 +182,7 @@
             this.ll_signup.BackColor = System.Drawing.Color.White;
             this.ll_signup.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ll_signup.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(157)))), ((int)(((byte)(162)))));
-            this.ll_signup.Location = new System.Drawing.Point(559, 700);
+            this.ll_signup.Location = new System.Drawing.Point(559, 924);
             this.ll_signup.Name = "ll_signup";
             this.ll_signup.Size = new System.Drawing.Size(96, 32);
             this.ll_signup.TabIndex = 35;
@@ -195,7 +195,7 @@
             this.lbl_account.BackColor = System.Drawing.Color.White;
             this.lbl_account.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.lbl_account.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(57)))), ((int)(((byte)(55)))));
-            this.lbl_account.Location = new System.Drawing.Point(258, 702);
+            this.lbl_account.Location = new System.Drawing.Point(258, 926);
             this.lbl_account.Name = "lbl_account";
             this.lbl_account.Size = new System.Drawing.Size(306, 32);
             this.lbl_account.TabIndex = 34;
@@ -212,55 +212,28 @@
             this.gunaPanel1.Size = new System.Drawing.Size(1540, 1600);
             this.gunaPanel1.TabIndex = 34;
             // 
-            // gunaShadowPanel1
+            // btn_minimize
             // 
-            this.gunaShadowPanel1.BaseColor = System.Drawing.Color.White;
-            this.gunaShadowPanel1.Controls.Add(this.btn_view1);
-            this.gunaShadowPanel1.Controls.Add(this.btn_hide1);
-            this.gunaShadowPanel1.Controls.Add(this.lbl_create_account);
-            this.gunaShadowPanel1.Controls.Add(this.ll_signup);
-            this.gunaShadowPanel1.Controls.Add(this.lbl_account);
-            this.gunaShadowPanel1.Controls.Add(this.txt_password);
-            this.gunaShadowPanel1.Controls.Add(this.btn_login);
-            this.gunaShadowPanel1.Controls.Add(this.lbl_email_error);
-            this.gunaShadowPanel1.Controls.Add(this.txt_email);
-            this.gunaShadowPanel1.Controls.Add(this.lbl_password);
-            this.gunaShadowPanel1.Controls.Add(this.lbl_password_error);
-            this.gunaShadowPanel1.Controls.Add(this.lbl_email);
-            this.gunaShadowPanel1.Location = new System.Drawing.Point(98, 358);
-            this.gunaShadowPanel1.Name = "gunaShadowPanel1";
-            this.gunaShadowPanel1.Radius = 3;
-            this.gunaShadowPanel1.ShadowColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(235)))), ((int)(((byte)(234)))));
-            this.gunaShadowPanel1.Size = new System.Drawing.Size(895, 858);
-            this.gunaShadowPanel1.TabIndex = 36;
-            // 
-            // btn_view1
-            // 
-            this.btn_view1.BackColor = System.Drawing.Color.White;
-            this.btn_view1.Image = global::CARES.Properties.Resources.view;
-            this.btn_view1.ImageSize = new System.Drawing.Size(18, 18);
-            this.btn_view1.Location = new System.Drawing.Point(686, 455);
-            this.btn_view1.Margin = new System.Windows.Forms.Padding(2);
-            this.btn_view1.Name = "btn_view1";
-            this.btn_view1.OnHoverImage = global::CARES.Properties.Resources.view_hover;
-            this.btn_view1.OnHoverImageOffset = new System.Drawing.Point(0, 0);
-            this.btn_view1.Size = new System.Drawing.Size(50, 50);
-            this.btn_view1.TabIndex = 26;
-            this.btn_view1.Click += new System.EventHandler(this.btn_view1_Click);
-            // 
-            // btn_hide1
-            // 
-            this.btn_hide1.BackColor = System.Drawing.Color.White;
-            this.btn_hide1.Image = global::CARES.Properties.Resources.hide;
-            this.btn_hide1.ImageSize = new System.Drawing.Size(18, 18);
-            this.btn_hide1.Location = new System.Drawing.Point(686, 455);
-            this.btn_hide1.Margin = new System.Windows.Forms.Padding(2);
-            this.btn_hide1.Name = "btn_hide1";
-            this.btn_hide1.OnHoverImage = global::CARES.Properties.Resources.hide_hover;
-            this.btn_hide1.OnHoverImageOffset = new System.Drawing.Point(0, 0);
-            this.btn_hide1.Size = new System.Drawing.Size(50, 50);
-            this.btn_hide1.TabIndex = 27;
-            this.btn_hide1.Click += new System.EventHandler(this.btn_hide1_Click);
+            this.btn_minimize.AnimationHoverSpeed = 0.07F;
+            this.btn_minimize.AnimationSpeed = 0.03F;
+            this.btn_minimize.BaseColor = System.Drawing.Color.Transparent;
+            this.btn_minimize.BorderColor = System.Drawing.Color.Black;
+            this.btn_minimize.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btn_minimize.ForeColor = System.Drawing.Color.White;
+            this.btn_minimize.Image = global::CARES.Properties.Resources.minimize;
+            this.btn_minimize.ImageAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.btn_minimize.ImageSize = new System.Drawing.Size(15, 15);
+            this.btn_minimize.Location = new System.Drawing.Point(1370, 11);
+            this.btn_minimize.Name = "btn_minimize";
+            this.btn_minimize.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(154)))), ((int)(((byte)(153)))));
+            this.btn_minimize.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.btn_minimize.OnHoverForeColor = System.Drawing.Color.White;
+            this.btn_minimize.OnHoverImage = global::CARES.Properties.Resources.minimize_hover;
+            this.btn_minimize.OnPressedColor = System.Drawing.Color.Black;
+            this.btn_minimize.Radius = 3;
+            this.btn_minimize.Size = new System.Drawing.Size(70, 55);
+            this.btn_minimize.TabIndex = 2;
+            this.btn_minimize.Click += new System.EventHandler(this.btn_minimize_Click);
             // 
             // btn_exit
             // 
@@ -297,28 +270,55 @@
             this.gunaTransfarantPictureBox1.TabIndex = 0;
             this.gunaTransfarantPictureBox1.TabStop = false;
             // 
-            // btn_minimize
+            // gunaShadowPanel1
             // 
-            this.btn_minimize.AnimationHoverSpeed = 0.07F;
-            this.btn_minimize.AnimationSpeed = 0.03F;
-            this.btn_minimize.BaseColor = System.Drawing.Color.Transparent;
-            this.btn_minimize.BorderColor = System.Drawing.Color.Black;
-            this.btn_minimize.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.btn_minimize.ForeColor = System.Drawing.Color.White;
-            this.btn_minimize.Image = global::CARES.Properties.Resources.minimize;
-            this.btn_minimize.ImageAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.btn_minimize.ImageSize = new System.Drawing.Size(15, 15);
-            this.btn_minimize.Location = new System.Drawing.Point(1370, 11);
-            this.btn_minimize.Name = "btn_minimize";
-            this.btn_minimize.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(154)))), ((int)(((byte)(153)))));
-            this.btn_minimize.OnHoverBorderColor = System.Drawing.Color.Black;
-            this.btn_minimize.OnHoverForeColor = System.Drawing.Color.White;
-            this.btn_minimize.OnHoverImage = global::CARES.Properties.Resources.minimize_hover;
-            this.btn_minimize.OnPressedColor = System.Drawing.Color.Black;
-            this.btn_minimize.Radius = 3;
-            this.btn_minimize.Size = new System.Drawing.Size(70, 55);
-            this.btn_minimize.TabIndex = 2;
-            this.btn_minimize.Click += new System.EventHandler(this.btn_minimize_Click);
+            this.gunaShadowPanel1.BaseColor = System.Drawing.Color.White;
+            this.gunaShadowPanel1.Controls.Add(this.btn_view1);
+            this.gunaShadowPanel1.Controls.Add(this.btn_hide1);
+            this.gunaShadowPanel1.Controls.Add(this.lbl_create_account);
+            this.gunaShadowPanel1.Controls.Add(this.ll_signup);
+            this.gunaShadowPanel1.Controls.Add(this.lbl_account);
+            this.gunaShadowPanel1.Controls.Add(this.txt_password);
+            this.gunaShadowPanel1.Controls.Add(this.btn_login);
+            this.gunaShadowPanel1.Controls.Add(this.lbl_email_error);
+            this.gunaShadowPanel1.Controls.Add(this.txt_email);
+            this.gunaShadowPanel1.Controls.Add(this.lbl_password);
+            this.gunaShadowPanel1.Controls.Add(this.lbl_password_error);
+            this.gunaShadowPanel1.Controls.Add(this.lbl_email);
+            this.gunaShadowPanel1.Location = new System.Drawing.Point(111, 100);
+            this.gunaShadowPanel1.Name = "gunaShadowPanel1";
+            this.gunaShadowPanel1.Radius = 3;
+            this.gunaShadowPanel1.ShadowColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(235)))), ((int)(((byte)(234)))));
+            this.gunaShadowPanel1.Size = new System.Drawing.Size(895, 1335);
+            this.gunaShadowPanel1.TabIndex = 36;
+            // 
+            // btn_view1
+            // 
+            this.btn_view1.BackColor = System.Drawing.Color.White;
+            this.btn_view1.Image = global::CARES.Properties.Resources.view;
+            this.btn_view1.ImageSize = new System.Drawing.Size(18, 18);
+            this.btn_view1.Location = new System.Drawing.Point(686, 679);
+            this.btn_view1.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_view1.Name = "btn_view1";
+            this.btn_view1.OnHoverImage = global::CARES.Properties.Resources.view_hover;
+            this.btn_view1.OnHoverImageOffset = new System.Drawing.Point(0, 0);
+            this.btn_view1.Size = new System.Drawing.Size(50, 50);
+            this.btn_view1.TabIndex = 26;
+            this.btn_view1.Click += new System.EventHandler(this.btn_view1_Click);
+            // 
+            // btn_hide1
+            // 
+            this.btn_hide1.BackColor = System.Drawing.Color.White;
+            this.btn_hide1.Image = global::CARES.Properties.Resources.hide;
+            this.btn_hide1.ImageSize = new System.Drawing.Size(18, 18);
+            this.btn_hide1.Location = new System.Drawing.Point(686, 679);
+            this.btn_hide1.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_hide1.Name = "btn_hide1";
+            this.btn_hide1.OnHoverImage = global::CARES.Properties.Resources.hide_hover;
+            this.btn_hide1.OnHoverImageOffset = new System.Drawing.Point(0, 0);
+            this.btn_hide1.Size = new System.Drawing.Size(50, 50);
+            this.btn_hide1.TabIndex = 27;
+            this.btn_hide1.Click += new System.EventHandler(this.btn_hide1_Click);
             // 
             // log_in
             // 
@@ -333,9 +333,9 @@
             this.Text = "log_in";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.gunaPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gunaTransfarantPictureBox1)).EndInit();
             this.gunaShadowPanel1.ResumeLayout(false);
             this.gunaShadowPanel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.gunaTransfarantPictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
