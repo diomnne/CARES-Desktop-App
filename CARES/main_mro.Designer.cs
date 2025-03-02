7f91ef503fb2defeb752eb2ed12b601648b05415
Frances Dionne Maglasang <dionnemaglasang@gmail.com>
2025-03-02 23:57:07 +0800

    Added Functions

--- CARES/main_mro.Designer.cs
+++ CARES/main_mro.Designer.cs
@@ -28,26 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pnl_sidebar = new Guna.UI.WinForms.GunaPanel();
+            this.pnl_navbar = new Guna.UI.WinForms.GunaPanel();
+            this.pnl_logo = new Guna.UI.WinForms.GunaPanel();
+            this.pnl_header = new Guna.UI.WinForms.GunaPanel();
+            this.pnl_form_container = new Guna.UI.WinForms.GunaPanel();
             this.btn_manage_records = new FontAwesome.Sharp.IconButton();
             this.btn_medical_records = new FontAwesome.Sharp.IconButton();
             this.btn_dashboard = new FontAwesome.Sharp.IconButton();
-            this.pnl_logo = new Guna.UI.WinForms.GunaPanel();
-            this.pnl_sidebar.SuspendLayout();
+            this.pnl_navbar.SuspendLayout();
             this.SuspendLayout();
             // 
-            // pnl_sidebar
+            // pnl_navbar
             // 
-            this.pnl_sidebar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(253)))), ((int)(((byte)(253)))));
-            this.pnl_sidebar.Controls.Add(this.btn_manage_records);
-            this.pnl_sidebar.Controls.Add(this.btn_medical_records);
-            this.pnl_sidebar.Controls.Add(this.btn_dashboard);
-            this.pnl_sidebar.Controls.Add(this.pnl_logo);
-            this.pnl_sidebar.Dock = System.Windows.Forms.DockStyle.Left;
-            this.pnl_sidebar.Location = new System.Drawing.Point(0, 0);
-            this.pnl_sidebar.Name = "pnl_sidebar";
-            this.pnl_sidebar.Size = new System.Drawing.Size(450, 1529);
-            this.pnl_sidebar.TabIndex = 0;
+            this.pnl_navbar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(253)))), ((int)(((byte)(253)))));
+            this.pnl_navbar.Controls.Add(this.btn_manage_records);
+            this.pnl_navbar.Controls.Add(this.btn_medical_records);
+            this.pnl_navbar.Controls.Add(this.btn_dashboard);
+            this.pnl_navbar.Controls.Add(this.pnl_logo);
+            this.pnl_navbar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnl_navbar.Location = new System.Drawing.Point(0, 0);
+            this.pnl_navbar.Name = "pnl_navbar";
+            this.pnl_navbar.Size = new System.Drawing.Size(480, 1529);
+            this.pnl_navbar.TabIndex = 0;
+            // 
+            // pnl_logo
+            // 
+            this.pnl_logo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnl_logo.Location = new System.Drawing.Point(0, 0);
+            this.pnl_logo.Name = "pnl_logo";
+            this.pnl_logo.Size = new System.Drawing.Size(480, 150);
+            this.pnl_logo.TabIndex = 0;
+            // 
+            // pnl_header
+            // 
+            this.pnl_header.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(253)))), ((int)(((byte)(253)))));
+            this.pnl_header.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnl_header.Location = new System.Drawing.Point(480, 0);
+            this.pnl_header.Name = "pnl_header";
+            this.pnl_header.Size = new System.Drawing.Size(2054, 150);
+            this.pnl_header.TabIndex = 1;
+            this.pnl_header.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnl_header_MouseDown);
+            // 
+            // pnl_form_container
+            // 
+            this.pnl_form_container.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(235)))), ((int)(((byte)(235)))));
+            this.pnl_form_container.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnl_form_container.Location = new System.Drawing.Point(480, 150);
+            this.pnl_form_container.Name = "pnl_form_container";
+            this.pnl_form_container.Size = new System.Drawing.Size(2054, 1379);
+            this.pnl_form_container.TabIndex = 3;
             // 
             // btn_manage_records
             // 
@@ -60,10 +89,10 @@
             this.btn_manage_records.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(157)))), ((int)(((byte)(162)))));
             this.btn_manage_records.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.btn_manage_records.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btn_manage_records.Location = new System.Drawing.Point(0, 400);
+            this.btn_manage_records.Location = new System.Drawing.Point(0, 410);
             this.btn_manage_records.Name = "btn_manage_records";
             this.btn_manage_records.Padding = new System.Windows.Forms.Padding(20, 0, 20, 0);
-            this.btn_manage_records.Size = new System.Drawing.Size(450, 100);
+            this.btn_manage_records.Size = new System.Drawing.Size(480, 130);
             this.btn_manage_records.TabIndex = 3;
             this.btn_manage_records.Text = "   Manage Records";
             this.btn_manage_records.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -82,10 +111,10 @@
             this.btn_medical_records.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(157)))), ((int)(((byte)(162)))));
             this.btn_medical_records.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.btn_medical_records.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btn_medical_records.Location = new System.Drawing.Point(0, 300);
+            this.btn_medical_records.Location = new System.Drawing.Point(0, 280);
             this.btn_medical_records.Name = "btn_medical_records";
             this.btn_medical_records.Padding = new System.Windows.Forms.Padding(20, 0, 20, 0);
-            this.btn_medical_records.Size = new System.Drawing.Size(450, 100);
+            this.btn_medical_records.Size = new System.Drawing.Size(480, 130);
             this.btn_medical_records.TabIndex = 2;
             this.btn_medical_records.Text = "   Medical Records";
             this.btn_medical_records.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -104,10 +133,10 @@
             this.btn_dashboard.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(157)))), ((int)(((byte)(162)))));
             this.btn_dashboard.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.btn_dashboard.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btn_dashboard.Location = new System.Drawing.Point(0, 200);
+            this.btn_dashboard.Location = new System.Drawing.Point(0, 150);
             this.btn_dashboard.Name = "btn_dashboard";
             this.btn_dashboard.Padding = new System.Windows.Forms.Padding(20, 0, 20, 0);
-            this.btn_dashboard.Size = new System.Drawing.Size(450, 100);
+            this.btn_dashboard.Size = new System.Drawing.Size(480, 130);
             this.btn_dashboard.TabIndex = 1;
             this.btn_dashboard.Text = "   Dashboard";
             this.btn_dashboard.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -115,35 +144,30 @@
             this.btn_dashboard.UseVisualStyleBackColor = true;
             this.btn_dashboard.Click += new System.EventHandler(this.btn_dashboard_Click);
             // 
-            // pnl_logo
-            // 
-            this.pnl_logo.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pnl_logo.Location = new System.Drawing.Point(0, 0);
-            this.pnl_logo.Name = "pnl_logo";
-            this.pnl_logo.Size = new System.Drawing.Size(450, 200);
-            this.pnl_logo.TabIndex = 0;
-            // 
             // main_mro
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(192F, 192F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(253)))), ((int)(((byte)(253)))));
             this.ClientSize = new System.Drawing.Size(2534, 1529);
-            this.Controls.Add(this.pnl_sidebar);
+            this.Controls.Add(this.pnl_form_container);
+            this.Controls.Add(this.pnl_header);
+            this.Controls.Add(this.pnl_navbar);
             this.Name = "main_mro";
             this.Text = "Main";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-            this.pnl_sidebar.ResumeLayout(false);
+            this.pnl_navbar.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private Guna.UI.WinForms.GunaPanel pnl_sidebar;
+        private Guna.UI.WinForms.GunaPanel pnl_navbar;
         private Guna.UI.WinForms.GunaPanel pnl_logo;
         private FontAwesome.Sharp.IconButton btn_dashboard;
         private FontAwesome.Sharp.IconButton btn_manage_records;
         private FontAwesome.Sharp.IconButton btn_medical_records;
+        private Guna.UI.WinForms.GunaPanel pnl_header;
+        private Guna.UI.WinForms.GunaPanel pnl_form_container;
     }
 }
