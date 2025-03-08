624338802666e11020c37908b182f47654cf97eb
Frances Dionne Maglasang <dionnemaglasang@gmail.com>
2025-03-08 21:53:29 +0800

    Add resource

--- CARES/Properties/Resources.Designer.cs
+++ CARES/Properties/Resources.Designer.cs
@@ -253,6 +253,16 @@ namespace CARES.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap refreshIcon1 {
+            get {
+                object obj = ResourceManager.GetObject("refreshIcon1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap scheduleIcon {
             get {
                 object obj = ResourceManager.GetObject("scheduleIcon", resourceCulture);
