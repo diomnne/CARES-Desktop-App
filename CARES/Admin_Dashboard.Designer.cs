8edbdbd00e7c185110cfee63c75f3355d3876325
Frances Dionne Maglasang <dionnemaglasang@gmail.com>
2025-03-09 14:46:08 +0800

    Add key metrics to admin dashboard

--- CARES/Admin_Dashboard.Designer.cs
+++ CARES/Admin_Dashboard.Designer.cs
@@ -30,6 +30,20 @@
         {
             this.lblDashboard = new System.Windows.Forms.Label();
             this.gunaLinePanel1 = new Guna.UI.WinForms.GunaLinePanel();
+            this.pnlRecentLogins = new Guna.UI.WinForms.GunaElipsePanel();
+            this.pnlPendingApprovals = new Guna.UI.WinForms.GunaElipsePanel();
+            this.pnlTotalUsers = new Guna.UI.WinForms.GunaElipsePanel();
+            this.lblUsersCtr = new Guna.UI.WinForms.GunaLabel();
+            this.lblTotalUsers = new Guna.UI.WinForms.GunaLabel();
+            this.lblRecentLogins = new Guna.UI.WinForms.GunaLabel();
+            this.lblRecentLoginsCtr = new Guna.UI.WinForms.GunaLabel();
+            this.lblPendingApprovals = new Guna.UI.WinForms.GunaLabel();
+            this.lblPendingApprovalsCtr = new Guna.UI.WinForms.GunaLabel();
+            this.gunaPictureBox1 = new Guna.UI.WinForms.GunaPictureBox();
+            this.pnlRecentLogins.SuspendLayout();
+            this.pnlPendingApprovals.SuspendLayout();
+            this.pnlTotalUsers.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gunaPictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // lblDashboard
@@ -55,17 +69,145 @@
             this.gunaLinePanel1.Size = new System.Drawing.Size(600, 1630);
             this.gunaLinePanel1.TabIndex = 4;
             // 
+            // pnlRecentLogins
+            // 
+            this.pnlRecentLogins.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(219)))), ((int)(((byte)(237)))));
+            this.pnlRecentLogins.Controls.Add(this.lblRecentLogins);
+            this.pnlRecentLogins.Controls.Add(this.lblRecentLoginsCtr);
+            this.pnlRecentLogins.Location = new System.Drawing.Point(510, 219);
+            this.pnlRecentLogins.Name = "pnlRecentLogins";
+            this.pnlRecentLogins.Radius = 8;
+            this.pnlRecentLogins.Size = new System.Drawing.Size(445, 250);
+            this.pnlRecentLogins.TabIndex = 5;
+            // 
+            // pnlPendingApprovals
+            // 
+            this.pnlPendingApprovals.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(173)))), ((int)(((byte)(219)))), ((int)(((byte)(218)))));
+            this.pnlPendingApprovals.Controls.Add(this.lblPendingApprovals);
+            this.pnlPendingApprovals.Controls.Add(this.lblPendingApprovalsCtr);
+            this.pnlPendingApprovals.Location = new System.Drawing.Point(982, 219);
+            this.pnlPendingApprovals.Name = "pnlPendingApprovals";
+            this.pnlPendingApprovals.Radius = 8;
+            this.pnlPendingApprovals.Size = new System.Drawing.Size(445, 250);
+            this.pnlPendingApprovals.TabIndex = 6;
+            // 
+            // pnlTotalUsers
+            // 
+            this.pnlTotalUsers.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(189)))), ((int)(((byte)(185)))));
+            this.pnlTotalUsers.Controls.Add(this.gunaPictureBox1);
+            this.pnlTotalUsers.Controls.Add(this.lblTotalUsers);
+            this.pnlTotalUsers.Controls.Add(this.lblUsersCtr);
+            this.pnlTotalUsers.Location = new System.Drawing.Point(38, 219);
+            this.pnlTotalUsers.Name = "pnlTotalUsers";
+            this.pnlTotalUsers.Radius = 8;
+            this.pnlTotalUsers.Size = new System.Drawing.Size(445, 250);
+            this.pnlTotalUsers.TabIndex = 7;
+            // 
+            // lblUsersCtr
+            // 
+            this.lblUsersCtr.AutoSize = true;
+            this.lblUsersCtr.BackColor = System.Drawing.Color.Transparent;
+            this.lblUsersCtr.Font = new System.Drawing.Font("Century Gothic", 19.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsersCtr.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(253)))), ((int)(((byte)(253)))));
+            this.lblUsersCtr.Location = new System.Drawing.Point(28, 66);
+            this.lblUsersCtr.Name = "lblUsersCtr";
+            this.lblUsersCtr.Size = new System.Drawing.Size(57, 63);
+            this.lblUsersCtr.TabIndex = 0;
+            this.lblUsersCtr.Text = "0";
+            // 
+            // lblTotalUsers
+            // 
+            this.lblTotalUsers.AutoSize = true;
+            this.lblTotalUsers.BackColor = System.Drawing.Color.Transparent;
+            this.lblTotalUsers.Font = new System.Drawing.Font("Century Gothic", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalUsers.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(253)))), ((int)(((byte)(253)))));
+            this.lblTotalUsers.Location = new System.Drawing.Point(34, 141);
+            this.lblTotalUsers.Name = "lblTotalUsers";
+            this.lblTotalUsers.Size = new System.Drawing.Size(148, 33);
+            this.lblTotalUsers.TabIndex = 1;
+            this.lblTotalUsers.Text = "Total Users";
+            // 
+            // lblRecentLogins
+            // 
+            this.lblRecentLogins.AutoSize = true;
+            this.lblRecentLogins.BackColor = System.Drawing.Color.Transparent;
+            this.lblRecentLogins.Font = new System.Drawing.Font("Century Gothic", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecentLogins.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(253)))), ((int)(((byte)(253)))));
+            this.lblRecentLogins.Location = new System.Drawing.Point(37, 141);
+            this.lblRecentLogins.Name = "lblRecentLogins";
+            this.lblRecentLogins.Size = new System.Drawing.Size(197, 33);
+            this.lblRecentLogins.TabIndex = 3;
+            this.lblRecentLogins.Text = "Recent Logins";
+            // 
+            // lblRecentLoginsCtr
+            // 
+            this.lblRecentLoginsCtr.AutoSize = true;
+            this.lblRecentLoginsCtr.BackColor = System.Drawing.Color.Transparent;
+            this.lblRecentLoginsCtr.Font = new System.Drawing.Font("Century Gothic", 19.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecentLoginsCtr.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(253)))), ((int)(((byte)(253)))));
+            this.lblRecentLoginsCtr.Location = new System.Drawing.Point(31, 66);
+            this.lblRecentLoginsCtr.Name = "lblRecentLoginsCtr";
+            this.lblRecentLoginsCtr.Size = new System.Drawing.Size(57, 63);
+            this.lblRecentLoginsCtr.TabIndex = 2;
+            this.lblRecentLoginsCtr.Text = "0";
+            // 
+            // lblPendingApprovals
+            // 
+            this.lblPendingApprovals.AutoSize = true;
+            this.lblPendingApprovals.BackColor = System.Drawing.Color.Transparent;
+            this.lblPendingApprovals.Font = new System.Drawing.Font("Century Gothic", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPendingApprovals.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(253)))), ((int)(((byte)(253)))));
+            this.lblPendingApprovals.Location = new System.Drawing.Point(34, 141);
+            this.lblPendingApprovals.Name = "lblPendingApprovals";
+            this.lblPendingApprovals.Size = new System.Drawing.Size(263, 33);
+            this.lblPendingApprovals.TabIndex = 9;
+            this.lblPendingApprovals.Text = "Pending Approvals";
+            // 
+            // lblPendingApprovalsCtr
+            // 
+            this.lblPendingApprovalsCtr.AutoSize = true;
+            this.lblPendingApprovalsCtr.BackColor = System.Drawing.Color.Transparent;
+            this.lblPendingApprovalsCtr.Font = new System.Drawing.Font("Century Gothic", 19.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPendingApprovalsCtr.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(253)))), ((int)(((byte)(253)))));
+            this.lblPendingApprovalsCtr.Location = new System.Drawing.Point(28, 66);
+            this.lblPendingApprovalsCtr.Name = "lblPendingApprovalsCtr";
+            this.lblPendingApprovalsCtr.Size = new System.Drawing.Size(57, 63);
+            this.lblPendingApprovalsCtr.TabIndex = 8;
+            this.lblPendingApprovalsCtr.Text = "0";
+            // 
+            // gunaPictureBox1
+            // 
+            this.gunaPictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.gunaPictureBox1.BaseColor = System.Drawing.Color.Transparent;
+            this.gunaPictureBox1.InitialImage = global::CARES.Properties.Resources.team;
+            this.gunaPictureBox1.Location = new System.Drawing.Point(265, 52);
+            this.gunaPictureBox1.Name = "gunaPictureBox1";
+            this.gunaPictureBox1.Size = new System.Drawing.Size(145, 145);
+            this.gunaPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.gunaPictureBox1.TabIndex = 2;
+            this.gunaPictureBox1.TabStop = false;
+            // 
             // Admin_Dashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(192F, 192F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(253)))), ((int)(((byte)(253)))));
             this.ClientSize = new System.Drawing.Size(2034, 1630);
+            this.Controls.Add(this.pnlTotalUsers);
+            this.Controls.Add(this.pnlPendingApprovals);
+            this.Controls.Add(this.pnlRecentLogins);
             this.Controls.Add(this.gunaLinePanel1);
             this.Controls.Add(this.lblDashboard);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Admin_Dashboard";
             this.Text = "Dashboard";
+            this.pnlRecentLogins.ResumeLayout(false);
+            this.pnlRecentLogins.PerformLayout();
+            this.pnlPendingApprovals.ResumeLayout(false);
+            this.pnlPendingApprovals.PerformLayout();
+            this.pnlTotalUsers.ResumeLayout(false);
+            this.pnlTotalUsers.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gunaPictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -75,5 +217,15 @@
 
         private System.Windows.Forms.Label lblDashboard;
         private Guna.UI.WinForms.GunaLinePanel gunaLinePanel1;
+        private Guna.UI.WinForms.GunaElipsePanel pnlRecentLogins;
+        private Guna.UI.WinForms.GunaElipsePanel pnlPendingApprovals;
+        private Guna.UI.WinForms.GunaElipsePanel pnlTotalUsers;
+        private Guna.UI.WinForms.GunaLabel lblTotalUsers;
+        private Guna.UI.WinForms.GunaLabel lblUsersCtr;
+        private Guna.UI.WinForms.GunaLabel lblRecentLogins;
+        private Guna.UI.WinForms.GunaLabel lblRecentLoginsCtr;
+        private Guna.UI.WinForms.GunaLabel lblPendingApprovals;
+        private Guna.UI.WinForms.GunaLabel lblPendingApprovalsCtr;
+        private Guna.UI.WinForms.GunaPictureBox gunaPictureBox1;
     }
 }
