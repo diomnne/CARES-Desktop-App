34f00a8ce4a3393d1a0fe4a8a1b6e865947e9412
Frances Dionne Maglasang <dionnemaglasang@gmail.com>
2025-03-11 17:00:03 +0800

    Feat(admin): add user management page

---
+++ CARES/Admin_ManageUsers.cs
@@ -0,0 +1,23 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CARES
+{
+    public partial class Admin_ManageUsers : Form
+    {
+        private string connectionString = "server=127.0.0.1;uid=root;pwd=20181024;database=cares_db";
+        public Admin_ManageUsers()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
