c176ca1218ce59cd8b22a3017dcbfb8178d4497e
Frances Dionne Maglasang <dionnemaglasang@gmail.com>
2025-03-14 09:48:49 +0800

    Change onstart form to signup

--- CARES/Program.cs
+++ CARES/Program.cs
@@ -16,7 +16,7 @@ namespace CARES
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Admin_Main());
+            Application.Run(new Signup());
         }
     }
 }
