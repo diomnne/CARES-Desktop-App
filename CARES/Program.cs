6ba700be68b2c154612fc70aacc3a59a0934b37e
Frances Dionne Maglasang <dionnemaglasang@gmail.com>
2025-03-15 12:09:08 +0800

    Change onstart form to admin main

--- CARES/Program.cs
+++ CARES/Program.cs
@@ -16,7 +16,7 @@ namespace CARES
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Signup());
+            Application.Run(new Admin_Main());
         }
     }
 }
